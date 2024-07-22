c76883f2749a8717e1713feac2df031e7548e3b8
Sahil Andhare <86720759+sahil-172002@users.noreply.github.com>
2024-07-22 15:38:22 +0530

    updated info in home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,66 +1,90 @@
-import Project from "../components/ui/Project"
-import { projects, skills } from "../data/data"
+import Project from "../components/ui/Project";
+import { projects, skills } from "../data/data";
 
 function Home() {
+  return (
+    <main>
+      <section className="mt-28 sm:mt-16 flex flex-col gap-4 items-start sm:flex-row-reverse sm:items-center justify-between">
+        <img
+          id="profilePic"
+          className="w-32 h-32 my-2"
+          src="./enderman.png"
+          alt="enderman"
+        />
 
-    return (
-        <main>
-            <section className="mt-28 sm:mt-16 flex flex-col gap-4 items-start sm:flex-row-reverse sm:items-center justify-between">
-                <img id="profilePic" className="w-32 h-32 my-2" src="./enderman.png" alt="enderman" />
+        <span>
+          <h1 className="text-3xl font-bold dark:text-gray-200 text-gray-800">
+           Sahil Andhare
+          </h1>
+          <p className="w-full max-w-sm mt-2 font-medium dark:text-gray-200 text-gray-600 text-lg leading-relaxed">
+           Web Dev & Designer | TCW Intern @GFG
+          </p>
+        </span>
+      </section>
 
-                <span>
-                    <h1 className="text-3xl font-bold dark:text-gray-200 text-gray-800">
-                        Hey, I&apos;m Sahil Andhare
-                    </h1>
-                    <p className="w-full max-w-sm mt-2 font-medium dark:text-gray-200 text-gray-600 text-lg leading-relaxed">
-                    I&apos;m a Web developer 
-                    </p>
-                </span>
-            </section>
+      <section className="mt-8">
+        <h3 className="text-xl font-bold dark:text-gray-200 text-gray-600">
+          Skills
+        </h3>
 
-            <section className="mt-8">
-                <h3 className="text-xl font-bold dark:text-gray-200 text-gray-600">
-                    Skills
-                </h3>
+        <ul className="flex flex-col sm:flex-row flex-wrap gap-2 mt-4 items-start">
+          {skills.map((skill) => (
+            <li
+              key={skill.description}
+              className="flex items-center gap-2 md:border border-gray-300 dark:md:border-gray-secondary md:p-3 rounded-lg w-full md:max-w-xs h-full md:h-20"
+            >
+              <i className={`${skill.iconClass} text-3xl`} alt="skillIcon" />
+              <p className="text-base font-medium dark:text-gray-200 text-gray-700">
+                {skill.description}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </section>
 
-                <ul className="flex flex-col sm:flex-row flex-wrap gap-2 mt-4 items-start">
-                    {
-                        skills.map(skill => (
-                            <li key={skill.description} className="flex items-center gap-2 md:border border-gray-300 dark:md:border-gray-secondary md:p-3 rounded-lg w-full md:max-w-xs h-full md:h-20">
-                                <i className={`${skill.iconClass} text-3xl`} alt="skillIcon" />
-                                <p className="text-base font-medium dark:text-gray-200 text-gray-700">
-                                    {skill.description}
-                                </p>
-                            </li>
-                        ))
-                    }
-                </ul>
-            </section>
+      <section className="mt-8 flex gap-4 flex-col justify-center w-full items-center">
+        <h3 className="text-xl font-bold dark:text-gray-200 text-gray-600">
+          My Work
+        </h3>
 
-            <section className="mt-8">
-                <h3 className="text-xl font-bold dark:text-gray-200 text-gray-600">
-                    My Work
-                </h3>
+       
+       <div className="flex flex-row justify-between items-center w-full flex-wrap">
+        <ul className="sm:flex-row flex-wrap gap-2 mt-4 items-start">
+          {projects.map((project) => (
+            <Project
+              key={project.id}
+              id={project.id}
+              title={project.title}
+              year={project.year}
+              description={project.description}
+              source={project.source}
+              demo={project.demo}
+              level={project.level}
+            />
+          ))}
+        </ul>
+   
+        <ul className="sm:flex-row flex-wrap gap-2 mt-4 items-start">
+          {projects.map((project) => (
+            <Project
+              key={project.id}
+              id={project.id}
+              title={project.title}
+              year={project.year}
+              description={project.description}
+              source={project.source}
+              demo={project.demo}   
+              level={project.level}
+            />
+          ))}
+        </ul>
 
-                <ul className="flex flex-col sm:flex-row flex-wrap gap-2 mt-4 items-start">
-                    {
-                        projects.map(project => (
-                            <Project
-                                key={project.id}
-                                id={project.id}
-                                title={project.title}
-                                year={project.year}
-                                description={project.description}
-                                source={project.source}
-                                demo={project.demo}
-                                level={project.level}
-                            />
-                        ))
-                    }
-                </ul>
-            </section>
-        </main>
-    )
+        
+        </div>
+
+      </section>
+    </main>
+  );    
 }
 
-export default Home
+export default Home;

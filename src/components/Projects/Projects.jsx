df1adc8d86dcc9153b61d304145579ac91ec69b8
sahil-172002 <sahil172002@gmail.com>
2024-09-24 12:11:29 +0530

    updated resume n pro

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import Card from "./Card";
-import MernChatApp from "../../assets/img/mernChatApp.png";
-import Soccer from "../../assets/img/soccer.png";
-import Blog from "../../assets/img/blog.png";
-import Social from "../../assets/img/social.png";
-import Bubble from "../../assets/img/bubble.png";
-import ChatApp from "../../assets/img/chatapp.png";
-import Todo from "../../assets/img/todo.png";
-import Spotify from "../../assets/img/spotify.png";
+import social from "../../assets/img/social.png";
+import g2g from "../../assets/img/g2g.png";
+import yt from "../../assets/img/yt.png";
+
 
 function Projects() {
     return (
@@ -20,7 +16,7 @@ function Projects() {
                 </div>
                 <div className="flex flex-wrap gap-2 sm:gap-14 justify-center md:justify-start md:mx-auto py-6 sm:py-0 w-full max-w-screen-lg  dark:bg-[#00091D]">
                     <Card
-                        src={Social}
+                        src={social}
                         name="AiQuizGen"
                         desc="AIQuizGen is an innovative web application designed to create customized quizzes from just one word."
                         T1="JS"
@@ -32,82 +28,28 @@ function Projects() {
                         GithubLink="https://github.com/sahil-172002/AIQuizGen"
                     />
                     <Card
-                        src={Blog}
-                        name="ClashofClans-Store"
+                        src={g2g}
+                        name="Gamers-Soul-Store"
                         desc="I developed web application designed to make buying and selling digital gaming assets effortless."
                         T1="Node"
                         T2="React"
                         T3="MongoDB"
                         btn="Get Code"
-                        LiveLink="#"
-                        GithubLink="https://github.com/sahil-172002"
+                        LiveLink="gamers-soul-store.vercel.app"
+                        GithubLink="https://github.com/sahil-172002/GamersSoul-Store"
                     />
                     <Card
-                        src={Blog}
-                        name="Tree Trop Tales - Blog App"
+                        src={yt}
+                        name="Youtube-Thumbnail Downloader"
                         desc="Developed a Blog Application in which users can post their though, ideas by writing blogs with images. "
                         T1="React"
                         T2="Tailwind CSS"
                         T3="Appwrite"
                         btn="Get Code"
-                        LiveLink="https://mega-blog-project-bice.vercel.app/"
-                        GithubLink="#"
+                        LiveLink="https://hd-youtube-thumbnail-download.vercel.app/"
+                        GithubLink="https://github.com/sahil-172002/YT-Thumbnail-Downloader"
                     />
-                    {/* <Card
-                        src={Soccer}
-                        name="Animating Website"
-                        desc="I've crafted a dynamic and engaging soccer club website. With smooth animations and transitions powered by GSAP."
-                        T1="JavaScript"
-                        T2="CSS"
-                        T3="GSAP"
-                        btn="Get Code"
-                        LiveLink="https://soccer-club-eta.vercel.app/"
-                        GithubLink="https://github.com/swamiabhishek45/Animating-Website"
-                    />
-                    <Card
-                        src={Bubble}
-                        name="Bubble Game"
-                        desc="Dive into a world of delightful distraction with our charming bubble game site! Crafted with love using HTML, CSS, and JavaScript"
-                        T1="HTML"
-                        T2="CSS"
-                        T3="JavaScript"
-                        btn="Get Code"
-                        LiveLink="https://bubble-bash-ten.vercel.app/"
-                        GithubLink="https://github.com/swamiabhishek45/Bubble-Game"
-                    />
-                    <Card
-                        src={ChatApp}
-                        name="Mystery Meet - Chat App"
-                        desc="Implemented a own chat app using socket.io library. As the name mystrey you can chat with any random person this is the uniqueness of this app."
-                        T1="HTML"
-                        T2="JavaScript"
-                        T3="Socket.IO"
-                        btn="Get Code"
-                        LiveLink=""
-                        GithubLink="https://github.com/swamiabhishek45/MysteryMeet-Chat-App"
-                    />
-                    <Card
-                        src={Todo}
-                        name="Add Todo"
-                        desc="Created a simple yet efficient to-do website. Storing to-dos locally using localStorage enhances accessibility and persistence."
-                        T1="React"
-                        T2="TailwindCSS"
-                        T3="Context API"
-                        btn="Get Code"
-                        LiveLink=""
-                        GithubLink="https://github.com/swamiabhishek45/React.js-Learning/tree/main/Projects/10todoContextLocal"
-                    />
-                    <Card
-                        src={Spotify}
-                        name="Spotify Clone"
-                        desc="This app provides you a smooth experience with responsiveness. It has songs that you like. This app is responsive in small devices also."
-                        T1="HTML"
-                        T2="CSS"
-                        T3="JavaScript"
-                        btn="Get Code"
-                        LiveLink=""
-                        GithubLink="https://github.com/swamiabhishek45/Spotify-Clone"
-                    /> */}
+                  
                 </div>
             </div>
         </>
